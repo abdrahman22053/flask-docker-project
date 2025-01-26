a9e7858291153e0bd9dce55c3b3c6421fb27c750
AbdoLegrand <abdototo4136@gmail.com>
2025-01-26 16:59:15 +0000

    Add bla bla

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    return 'Hello, Docker ! This is a simple Flask web application running in a Docker container.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
